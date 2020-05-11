53efe54749cba1624dfd8181537306100077a2e3
kento.tokumoto <tokumoto@lassic.co.jp>
2020-05-11 04:54:16 +0000

    user login

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,11 +23,7 @@ class CreateUsersTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+    
     public function down()
     {
         Schema::dropIfExists('users');
